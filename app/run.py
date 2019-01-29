6734bd2811ed3d99c82907c64d58f2ab22bd3b29
ssbajk <L@casadepape!>
2019-01-29 13:46:30 -0500

    Mounting Json

--- app/run.py
+++ app/run.py
@@ -1,2 +1,2 @@
 from app import app
-app.run(host='127.0.0.1', port='80', debug=True, use_reloader=True)
+app.run(host='0.0.0.0', port='6060', debug=True, use_reloader=True)
--- app/views.py
+++ app/views.py
@@ -11,3 +11,19 @@ def initialpage():
 @app.route('/joinus')
 def charts():
     return render_template('joinus.html', title='Charts')
+
+@app.route('/register', methods=['POST', 'GET'])
+def getForm():
+    name = request.form['name']
+    born = request.form['born']
+    nationality = request.form['nationality']
+    email = request.form['email']
+    phone = request.form['phone']
+    description = request.form['description']
+    skills = request.form['skills']
+    experience = request.form['experience']
+
+    json =
+    print(name, born, nationality, email, phone, description, skills, experience)
+
+    return render_template('register.html', title='Register')
